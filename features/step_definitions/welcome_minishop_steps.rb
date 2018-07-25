362a3d6de6d6d209267501bdca99352d0419f795
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-25 21:25:56 +1200

    Lay down the step definations for welcome_minishop feature

---
+++ features/step_definitions/welcome_minishop_steps.rb
@@ -0,0 +1,9 @@
+Given("User is on welcome page") do
+  visit root_path
+end
+When("User clicks the {string} Button") do |btn|
+  click_button(btn)
+end
+Then("User should see {string}") do |content|
+  expect(page).to have_content(content)
+end
