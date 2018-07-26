3961e4b24d5a1a33c11041ef1a90451465c3af10
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-27 09:45:51 +1200

    Test the root route using shoulda matcher

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-
-  describe "GET #index" do
-    it "succeeds" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+  it { is_expected.to route(:get,'welcome/index').to(action: :index) }
 end
