338e14b9dd6caa5c151d6bb75e1a8c88094f77d2
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-27 11:02:14 +1200

    Add a resource routing for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
   get 'welcome/index'
   root 'welcome#index'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
