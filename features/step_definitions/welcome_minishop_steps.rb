35a8973798e2f856b04a452682100a4c4167dcc4
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-26 16:23:04 +1200

    Update the step definations corresponding to change to scenario

--- features/step_definitions/welcome_minishop_steps.rb
+++ features/step_definitions/welcome_minishop_steps.rb
@@ -2,10 +2,6 @@ Given("User is on welcome page") do
   visit root_path
 end
 
-When("User clicks the {string} Button") do |btn|
-  click_button(btn)
-end
-
 Then("User should see {string}") do |content|
   expect(page).to have_content(content)
 end
