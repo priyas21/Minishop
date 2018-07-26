e1fa0ffbefa6dbadf48ff79b51c7683d1710caa5
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-26 15:54:24 +1200

    Rename the it block

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "succeeds" do
       get :index
       expect(response).to have_http_status(:success)
     end
