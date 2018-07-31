bb2d9ea98328a3f6d9da89d92efbe814fe66e8f9
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-31 15:16:12 +1200

    Add validation for presence of first name and test it
    It will varify that a first name is present or not
    
    The test will state the user is not valid if the first_name
    is empty

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.new(:first_name => first_name, :last_name => last_name, :email => email) }
+  let(:first_name) { "Priya" }
+  let(:last_name) { "Sodhi" }
+  let(:email) { "p@example.com" }
+  context "without first name" do
+    let(:first_name) { nil }
+
+      it "is not valid" do
+      expect(user).to_not be_valid
+    end
+  end
 end
