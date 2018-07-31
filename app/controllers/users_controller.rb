3f9bf8ec09d1e58a6ac94d42b66cf2666b3c9df4
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-01 10:51:55 +1200

    Add a create action
    Create will create a new user and save it in the database
    After it saved successfully it will display the flash message
    and redirect to show page
    
    It the user is not saved successfully, it will display alert message
    and will render the form again
    
    Create the new user by passing the parameters also know as strong parameters
    submitted by the user on the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User is created successfully"
+      redirect_to @user
+    else
+      flash[:danger] = "Please enter valid information"
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :email)
   end
 end
