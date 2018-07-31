535f52cb512a36c3166b4f2063edfc4cd32f03fc
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-31 15:24:34 +1200

    Validate the format of email and test it
    Validate the format against an email regex
    
    Test the valid and invalid email addresses format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z+]+\z/i
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true ,
+            format: { with: VALID_EMAIL_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,4 +29,28 @@ RSpec.describe User, type: :model do
       expect(user).to_not be_valid
     end
   end
+  context "with invalid email address" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz...com]
+
+    it "is not valid" do
+      invalid_addresses.each do |invalid_address|
+        user.email = invalid_address
+        expect(user).to_not be_valid
+      end
+    end
+  end
+
+  context "with valid email address" do
+    valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+
+    it "is valid" do
+      valid_addresses.each do |valid_address|
+        user.email = valid_address
+        expect(user).to be_valid
+      end
+    end
+  end
+
 end
