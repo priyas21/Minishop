9ee9f743df9f7a10f1d602b56511dda6535b4cd9
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-01 15:17:48 +1200

    Test the new action
    Remove the test for rendering new template
    
    The test will accepts the class and checks
    whether user is the instance of User class or not

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  subject(:user) { User.create!(:first_name => first_name, :last_name => last_name, :phone => phone, :email => email) }
+  let(:first_name) { "Priya" }
+  let(:last_name) { "Sodhi" }
+  let(:phone) { "0226258409" }
+  let(:email) { "p@gmail.com" }
   describe "#new" do
-    it "renders the new template" do
-      get :new
-      expect(response).to render_template(:new)
+    context "when initialized" do
+      before { get :new }
+
+      it "is a new user" do
+       expect(assigns(:user)).to be_a_new(User)
+      end
     end
   end
 
