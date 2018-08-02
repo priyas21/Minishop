325a7270219b13224a3826fd033dfc916da2549b
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-03 11:59:54 +1200

    Add addresses as nested resource inside users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
-  resources :users
+  resources :users do
+    resources :addresses
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
