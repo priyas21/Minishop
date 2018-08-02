c9d30a0854444b501e9d40709b9a7a38a01b2e7b
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-03 11:41:00 +1200

    Add presence and length validations to address fields

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :address1, presence: true, length: { maximum: 120 }
+  validates :city, presence: true, length: { maximum: 120 }
+  validates :district, presence: true, length: { maximum: 100 }
+  validates :suburb, presence: true, length: { maximum: 100 }
+  validates :post_code, presence: true, length: { maximum: 16 }
 end
