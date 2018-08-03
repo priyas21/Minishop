1c35a74dfcaa3e81b3e98657581ba84b2fde902d
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-03 12:00:10 +1200

    Save the address of the user into the addresses table
    When user enters the invalid information it will display the errors

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,15 @@
 class AddressesController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @address = @user.addresses.create(address_params)
+    if @address.errors.any?
+      flash[:notice] = @address.errors.full_messages.to_sentence
+    end
+    redirect_to user_path(@user)
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:address1, :address2, :city, :district, :suburb, :post_code)
+  end
 end
