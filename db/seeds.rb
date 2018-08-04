6e6d7c9f720498aeabe83a4f43ab50f90b71dfaa
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-04 16:00:06 +1200

    Rollbacked seeding of data into registry table(WIP)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-registries = Registry.create([{
-  address: "43 Hanson st Newtown Wellington",
-  icp: "123456789" },
-  { address: "14A Beach st Somgeville Auckland",
-  icp: "951648237" },
-  { address: "8 Woodvile road Lesston Canterbury",
-  icp: "984EF5761" },
-  { address: "86 Customhouse Quay Wellington",
-  icp: "144476500" }
-])
 
