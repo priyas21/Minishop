8249ba0dc007dffb1d100c6dc6c06613465111dc
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-04 16:36:16 +1200

    Add the named routes to login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   get 'welcome/index'
   root 'welcome#index'
+
+  get     '/login', to: 'sessions#new'
+  post    '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users do
     resources :addresses
