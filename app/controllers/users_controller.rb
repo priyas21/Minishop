a114bc66fccda34421ec0741a505e6b45d8a0e92
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-05 22:15:03 +1200

    Add the edit action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,9 @@ class UsersController < ApplicationController
     end
   end
 
+    def edit
+    end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :phone, :email)
