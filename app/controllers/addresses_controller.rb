611b22d4a29004756b1adeb461a30973bec9dc3a
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-05 23:09:51 +1200

    Require logged_in user to edit and update their details
    before_action filter is used before edit and update action,
    that is user has to be logged in before in order to
    perform any editing or updating of their details

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,5 @@
 class AddressesController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def create
     @user = User.find(params[:user_id])
     @address = @user.addresses.create(address_params)
@@ -29,5 +30,11 @@ class AddressesController < ApplicationController
   def address_params
     params.require(:address).permit(:address1, :address2, :city, :district, :suburb, :post_code, :icp_number)
   end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def index
     @users = User.all
   end
@@ -38,5 +39,12 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :phone, :email)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_path
+    end
   end
 end
