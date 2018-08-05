3a62e5adf7d139c0587939e73887ac7aaf1f7462
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-05 22:39:47 +1200

    Log in the user upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       flash.now[:success] = "User is created successfully"
       redirect_to @user
     else
