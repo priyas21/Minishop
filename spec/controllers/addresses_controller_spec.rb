70728f56a55ec9eb1ef0cd3d8a427c430f42e777
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-08 11:27:25 +1200

    Test create action of addresses controller with valid attributes

--- spec/controllers/addresses_controller_spec.rb
+++ spec/controllers/addresses_controller_spec.rb
@@ -2,4 +2,34 @@ require 'rails_helper'
 
 RSpec.describe AddressesController, type: :controller do
 
+  let!(:user) { User.create!(:first_name => first_name, :last_name => last_name, :phone => phone, :email => email) }
+  let(:first_name) { "Priya" }
+  let(:last_name) { "Sodhi" }
+  let(:phone) { "0226258409" }
+  let(:email) { "p@gmail.com" }
+
+  describe "#create" do
+    let(:create_address) { post :create, :params => address_params }
+    let(:address_params) { { :address => { :address1 => address1, :address2 => address2, :city => city,
+      :district => district, :suburb => suburb, :post_code => post_code, :icp_number => icp_number }, :user_id => user.to_param } }
+    let(:address1) { "19A mahoe st" }
+    let(:address2) { "" }
+    let(:city) { "Wellington" }
+    let(:district) { "Porirua" }
+    let(:suburb) { "Tawa" }
+    let(:post_code) { "1111" }
+    let(:icp_number) { "12sdnkli54" }
+
+    context "with valid attributes" do
+
+      it "creates a new address" do
+        expect{create_address}.to change{Address.count}.by(1)
+      end
+      it "redirects to addresses show template" do
+        expect(create_address).to redirect_to :action => :show, :id => assigns(:address).id
+      end
+    end
+
+  end
+
 end
