828c268395c820cd8afc0829280a89e83ddce3bc
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-07 14:54:33 +1200

    Update the edited user's details by user
    When admin updates the user's details he will be redirected to view the
    details. If not successfull the edit page will be rendered again
    
    Private method set_user is craeted to refactor the repeated code used
    in edit and update actions
    
    Strong paramters are prmitted using address_params method

--- app/controllers/admin/addresses_controller.rb
+++ app/controllers/admin/addresses_controller.rb
@@ -1,5 +1,6 @@
 class Admin::AddressesController < ApplicationController
-  before_action :require_admin, only: [:index, :edit]
+  before_action :set_user, only: [:edit, :update]
+  before_action :require_admin, only: [:index, :edit, :update]
 
   def index
     @users = User.where(admin: false)
@@ -9,10 +10,27 @@ class Admin::AddressesController < ApplicationController
 
   end
 
+  def update
+    if @address.update_attributes(address_params)
+      redirect_to admin_address_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def require_admin
     unless current_user.admin?
       redirect_to root_path
     end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+    @address = @user.addresses.find(params[:id])
+  end
+
+   def address_params
+    params.require(:address).permit(:address1, :address2, :city, :district, :suburb, :post_code, :icp_number, :approval_status)
   end
 end
