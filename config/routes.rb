f89e1b0a7842a8975caddfef246bfa5b6e3e6dca
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-07 14:06:31 +1200

    Create admin namespace
    admin namespace groups users and addresses controller under it
    to provide some administrative tasks

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   resources :users do
     resources :addresses
   end
+
+  namespace :admin do
+    resources :users, :addresses
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
