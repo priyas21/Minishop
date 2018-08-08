b6817c7f7aab3a926293ee512f0590d17ff0ad9c
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-08 19:02:40 +1200

    The form will display the errors if not submitted with valid values
    After displaying the errors, form will be redirected to same page
    
    If submitted successfully, the address will be created and form
    will be redirected to show page displaying user details

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,9 +3,11 @@ class AddressesController < ApplicationController
     @user = User.find(params[:user_id])
     @address = @user.addresses.create(address_params)
     if @address.errors.any?
-      flash[:notice] = @address.errors.full_messages.to_sentence
+      flash[:danger] = @address.errors.full_messages.to_sentence
+      redirect_to user_path(@user)
+    else
+      redirect_to user_address_path(@user, @address)
     end
-    redirect_to user_address_path(@user, @address)
   end
 
   def show
