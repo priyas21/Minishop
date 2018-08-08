83ae555f074942934431ee273ba4dc843eea9fdd
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-08 13:46:10 +1200

    Delete associated object address when a user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :addresses
+  has_many :addresses, dependent: :destroy
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z+]+\z/i
 
