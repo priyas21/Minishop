abcf89c8e782e1d5329bcd334df4cf1fffc218a3
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-16 11:58:26 +1200

    Create new register serials for selected user and their address
    Before creating new register serial find_id method will grab the current
    user and address id

---
+++ app/controllers/admin/register_serials_controller.rb
@@ -0,0 +1,29 @@
+class Admin::RegisterSerialsController < ApplicationController
+  before_action :find_id, only: [ :new, :create]
+
+  def new
+    @register_serial = RegisterSerial.new
+  end
+
+  def create
+    @register_serial = @address.icp.register_serials.new(register_params)
+    if @register_serial.save
+      flash[:success] = "Register serial added successfully"
+      redirect_to admin_user_address_icp_register_serial_path(@user, @address, @address.icp, @register_serial)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def register_params
+    params.require(:register_serial).permit(:meter_number, :register_number, :register_decimals)
+  end
+
+  def find_id
+    @user = User.find(params[:user_id])
+    @address = Address.find(params[:address_id])
+  end
+
+end
