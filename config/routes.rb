c69ae20ca977f312ec0168a5c9e9115ac99daf7e
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-15 23:06:12 +1200

    Update the namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,13 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :users, :addresses
+    resources :users do
+      resources :addresses do
+        resources :icps do
+          resources :register_serials
+        end
+      end
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
