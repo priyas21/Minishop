55422a36fc1e6ab21be558dfccd0f140f5b8e4e6
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-16 11:35:09 +1200

    Allow only admin to view all users
    Add a private function which will check whether the current user is admin or not

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,13 @@
 class Admin::UsersController < ApplicationController
+  before_action :require_admin, only: [ :index ]
  def index
     @users = User.where(admin: false)
+  end
+
+  private
+  def require_admin
+    unless current_user.admin?
+      redirect_to root_path
+    end
   end
 end
