ab70ab55ef71eee49c4ab39e1c3a66ccec9935fa
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-16 09:44:05 +1200

    Allow user to create address and its associated icp number
    Use nested forms to allow user to add icp number

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,10 @@ class Address < ApplicationRecord
   belongs_to :icp
 
   accepts_nested_attributes_for :icp
+  after_initialize do
+    self.icp ||= self.build_icp()
+  end
+
   validates :address1, presence: true
   validates :city, presence: true
   validates :district, presence: true
