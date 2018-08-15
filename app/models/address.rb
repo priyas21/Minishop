e2ac5d6dffc44e8c1b3b69e8ce2e2ac3818971b5
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-16 09:42:18 +1200

    Add nested attribute for icp number in addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,7 @@ class Address < ApplicationRecord
   belongs_to :user
   belongs_to :icp
 
+  accepts_nested_attributes_for :icp
   validates :address1, presence: true
   validates :city, presence: true
   validates :district, presence: true
