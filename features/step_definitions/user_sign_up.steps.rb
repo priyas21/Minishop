26a0b05deddb88fa3a6c41fa160c06c46c6e7806
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-16 17:37:43 +1200

    Add the step definations for user sign up feature

--- features/step_definitions/user_sign_up.steps.rb
+++ features/step_definitions/user_sign_up.steps.rb
@@ -1,9 +1,29 @@
 Given("I am on signup page") do
   visit new_user_path
 end
-When("I fills in {string} with {string}") do |field, value|
+
+When("I fill in {string} with {string}") do |field, value|
   fill_in(field, :with => value)
 end
-When("I clicks the {string} button") do |btn|
+When("I click on {string}") do |btn|
   click_button(btn)
+end
+
+When("I enter my personal details") do
+  steps %{
+    When I fill in "First name" with "p21"
+     And I fill in "Surname" with "s"
+     And I fill in "Contact phone" with "1234567898"
+     And I fill in "Email address" with "lilly@example.com"
+  }
+end
+
+When("I enter my property details") do
+  steps %{
+    When I fill in "Address 1" with "19A mahoe st"
+     And I fill in "City" with "Wellington"
+     And I fill in "District" with "Porirua"
+     And I fill in "Suburb" with "Tawa"
+     And I fill in "Post code" with "1234"
+  }
 end
