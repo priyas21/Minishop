d3deb907ed9df3dbe85791ca79894ed969e32978
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-16 17:36:29 +1200

    Update the minimum length validation for phone numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :phone, presence: true,
-            numericality: true, length: { minimum: 10, maximum: 15 }
+            numericality: true, length: { minimum: 5 , maximum: 15 }
   validates :email, presence: true , length: { maximum: 255},
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
