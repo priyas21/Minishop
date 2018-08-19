c088fd10eb022dff7303ff175b46efcf9af87469
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-20 10:50:13 +1200

    Update the path to redirect to when user log in
    Once the user log in, they will be redirected to a page
    where all their properties are displayed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user
       log_in(user)
-      address = current_user.addresses.find_by(user_id: current_user.id)
       if current_user.admin?
         redirect_to admin_users_path
       else
-        redirect_to user_address_path(current_user, address)
+        redirect_to user_addresses_path(current_user)
       end
     else
       flash.now[:danger] = "Invalid email addrress."
