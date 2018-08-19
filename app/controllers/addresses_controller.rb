3c47c69decf4415c1222af14ef008255087254d8
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-20 10:47:38 +1200

    Allow the user to view all his properties
    Add an index action which will display all the
    properties of user once he logged in

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,11 @@
 class AddressesController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_address, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_address, only: [:index, :edit, :update]
+
+  def index
+    @user = User.find(params[:user_id])
+    @addresses = @user.addresses.all
+  end
 
   def create
     @user = User.find(params[:user_id])
