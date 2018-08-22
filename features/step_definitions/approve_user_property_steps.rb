194839660df4e072cf90c3ad1462a784c3826d2e
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-22 14:15:22 +1200

    Update step defination for approve_user_property feature

--- features/step_definitions/approve_user_property_steps.rb
+++ features/step_definitions/approve_user_property_steps.rb
@@ -4,6 +4,14 @@ Given("Admin is logged in") do
   click_button("Log in")
 end
 
-When("I check the {string} checkbox") do |approved|
-  check(approved)
+When("I check the Approved checkbox") do
+  find("#address_approval_status").click
+end
+
+When("I update the user address") do
+  steps %{
+    When I fill in "Address 1" with "13 Mahoe Street"
+     And I fill in "Post code" with "5028"
+     And I fill in "ICP Number" with "123ED3456"
+  }
 end
