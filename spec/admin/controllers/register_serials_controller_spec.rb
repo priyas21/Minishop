78f71da3bdf221dd017ddda76a58bcaedf6ae932
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-08-28 13:48:47 +1200

    Laid down the describe blocks to test the actions of register serials controller

---
+++ spec/admin/controllers/register_serials_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Admin::RegisterSerialsController, type: :controller do
+
+  describe "#new" do
+  end
+
+  describe "#create" do
+  end
+
+  describe "#index" do
+  end
+
+  describe "#edit" do
+  end
+
+  describe "#update" do
+  end
+
+  describe "#show" do
+  end
+
+  describe "#destroy" do
+  end
+end
